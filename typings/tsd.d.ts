4ee062d509992084609d00d2100420e0b52c2ed3
Christopher Martin <Christopher.Martin@disney.com>
2015-11-22 01:17:49 -0500

    fix leaky branch

--- typings/tsd.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="angular-ui-router/angular-ui-router.d.ts" />
-/// <reference path="angularjs/angular-sanitize.d.ts" />
-/// <reference path="angularjs/angular.d.ts" />
-/// <reference path="jquery/jquery.d.ts" />
